01efe702e420acc6eb2a6200b96b3e87120d51eb
parsa <parsa.eskandar@gmail.com>
2021-04-16 21:46:20 +0430

    show_user implemented

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from http import HTTPStatus
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
@@ -14,21 +15,29 @@ class Todo(db.Model):
     number = db.Column(db.String(11), nullable=False)
 
 
-
 @app.route('/signup')
 def signup():
-	data = request.json
-	new_user = Todo(username=data['username'], password=data['password'], email=data['email'], number=data['number'])
+    data = request.json
+    new_user = Todo(username=data['username'], password=data['password'], email=data['email'], number=data['number'])
 
-	try:
-		db.session.add(new_user)
-		db.session.commit()
-		return {'message': 'Success'}, HTTPStatus.CREATED	
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return {'message': 'Success'}, HTTPStatus.CREATED
 
-	except:
-		return {'message': 'Error: bad request'}, HTTPStatus.BAD_REQUEST
-    
+    except:
+        return {'message': 'Error: bad request'}, HTTPStatus.BAD_REQUEST
 
+
+@app.route('/show_user', methods=['GET'])
+def show_user(username):
+    if request.method == 'GET':
+        user = Todo.query.filter_by(username=username)
+
+        try:
+            return {'message': 'Success', 'user': user}, HTTPStatus.OK
+        except:
+            return {'message': 'Error'}, HTTPStatus.NOT_FOUND
 
 
 if __name__ == "__main__":
