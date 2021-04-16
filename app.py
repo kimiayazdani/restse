a859553689f6cea384f0a11954f66ca8b3820a6b
parsa <parsa.eskandar@gmail.com>
2021-04-16 20:53:43 +0430

    Start Flask project and create database by sqlalchemy for it

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+
+@app.route('/')
+def index():
+    return "something"
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
