f55bb22ed25dd2c32355576a87e54e04f3ce661f
Kim <yazdanikimia@yahoo.com>
2021-04-16 21:28:06 +0430

    create user

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy, insert 
-
+from flask_sqlalchemy import SQLAlchemy
+from http import HTTPStatus
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+tokens = {}
 
 
 class Todo(db.Model):
     username = db.Column(db.String(200), primary_key=True)
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False)
-    number = db.Coumn(db.String(11), nullable=False)
+    number = db.Column(db.String(11), nullable=False)
 
 
 
-@app.route('/')
-def index():
-    return "something"
+@app.route('/signup')
+def signup():
+	data = request.json
+	new_user = Todo(username=data['username'], password=data['password'], email=data['email'], number=data['number'])
+
+	try:
+		db.session.add(new_user)
+		db.session.commit()
+		return {'message': 'Success'}, HTTPStatus.CREATED	
+		
+	except:
+		return {'message': 'Error: missing or bad fields'}, HTTPStatus.BAD_REQUEST
+    
+
 
 
 if __name__ == "__main__":
